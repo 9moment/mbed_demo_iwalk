1d90728dfc00aa2157a1af2850702df5acf3fc50
makia <makia@localhost>
2017-12-19 09:03:07 +0000

    add state varlu

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include "mbed.h"
 
-
 InterruptIn button(USER_BUTTON);
 Serial pc(USBTX, USBRX); //TX, RX
 Thread thread;
 DigitalOut led(LED1);
 
-
-int speed = 0;//ค่าเริ่มต้นเป็น0
-double delay = 0.5; // 500 ms
+int speed = 0; //ค่าเริ่มต้นเป็น0
+int state = 0; // 0 = off, 1 = on
 
 void tx_thread() {
     while (true) {
-        pc.printf("test %d \n", speed); //ไว้ดูข้อมูลที่จะส่ง
+        pc.printf("test %d %d \n", speed,state); //ไว้ดูข้อมูลที่จะส่ง
         wait(0.1);
         wait(60);
     }
@@ -24,6 +22,11 @@ void interrup()
     wait(0.1);
     if(speed == 11){
         speed = 0 ;
+    }
+    if(speed != 0){
+        state = 1;
+    }else{
+        state = 0;
     }
     wait(0.1);
 }
@@ -37,7 +40,7 @@ int main()
     
     while (1) {
         led = !led;
-        pc.printf("%d" , speed);
+        pc.printf("%d , %d" , speed,state);
         wait(1);
     }
 }
